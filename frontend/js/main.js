82addf3fa80e9fd271f4503e3bf0c0189c827034
LCHansson <love.hansson@gmail.com>
2015-03-30 00:33:49 +0200

    Remove old crud from JS code

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,7 +5,7 @@
 
 function addDialog(event, data, caller) {
     var $dialog = $('#element-selector'),
-        //$dialog_content = $("#element-selector-content")
+    //$dialog_content = $("#element-selector-content")
         x = event.pageX,
         y = event.pageY;
 
@@ -16,25 +16,20 @@ function addDialog(event, data, caller) {
 
     data.forEach(function(el) {
         $dialog.append('<span class="callback-element">' + el + '</span><br/>');
-        //$dialog.append("Test");
     });
 
     $(".callback-element").click(function(e) {
         $(caller).text($(this).text());
-        //console.log(this);
     })
 
     // Show modal
     if(!$($dialog).is(':visible'))
     {
         $($dialog).show(100);
-        //$($dialog_content).delay(150).show(400);
     }
 
     // Positioning
     $dialog.css("left", x).css("top", y);
-    //$dialog_content.css("left", x).css("top", y);
-
 }
 
 $("#element-selector").click( function () {
@@ -42,7 +37,6 @@ $("#element-selector").click( function () {
     if($("#element-selector").is(':visible'))
     {
         $("#element-selector").delay(200).hide(100);
-        //$("#element-selector-content").hide(200);
     }
 });
 
@@ -72,14 +66,7 @@ function setupggDOM (data) {
     $(".data").click(function(event) {
         console.log("Clicked a data set name.");
         var allowed_datasets = $(this).data("allowed_datasets");
-        //var current_data = $(this).text(),
-        //    data_pos = allowed_datasets.indexOf(current_data),
-        //    next_pos = data_pos + 1 > allowed_datasets.length - 1
-        //        ? data_pos + 1 - allowed_datasets.length
-        //        : data_pos + 1,
-        //    next_data = allowed_datasets[next_pos];
 
-        //$(this).text(next_data);
         addDialog(event, allowed_datasets, this);
 
         // TODO:
@@ -109,18 +96,7 @@ function setupggDOM (data) {
             //    - getAllowedAes();
         }
 
-        //console.log("attr('data-type'): " + $(this).attr('data-type'));
-
-        //var name_pos = allowed_aes.indexOf(current_aesthetic);
-
-            //current_aesthetic = $(this).text(),
-            //next_pos = name_pos + 1 > allowed_aes.length - 1
-            //    ? name_pos + 1 - allowed_aes.length
-            //    : name_pos + 1,
-            //next_name = allowed_aes[next_pos];
-
         console.log("Clicked an aes parameter.\n\tActive geoms: " + active_geoms + "\n\tAllowed aesthetics: " + allowed_aes);
-        //+ "\n\tCurrent aesthetic: " + current_aesthetic+ "\n\tNext name: " + next_name);
 
         //$(this).text(next_name);
         addDialog(event, allowed_aes, this);
@@ -128,21 +104,6 @@ function setupggDOM (data) {
         // modifyAesArgument();
         //var valid_arguments = getValidAesArguments(current_aesthetic);
         //updateAesArgument(valid_arguments);
-
-
-        //current_name = $(this).text(),
-        //name_pos = data_names.indexOf(current_name),
-        //next_pos = name_pos + 1 > data_names.length - 1
-        //    ? name_pos + 1 - data_names.length
-        //    : name_pos + 1,
-        //next_name = data_names[next_pos];
-
-        //console.log("You have clicked an aes attribute that is a variable name.\n\tCurrent dataset: " + active_dataset + "\n\tAvailable names: " + data_names + "\n\tCurrent name: " + current_name  + "\n\tNext name: " + next_name);
-        //$(this).text(next_name);
-        //console.log("Active geoms: " + active_geoms + "\n\tAllowed aesthetics: " + allowed_aes);
-        //}
-        //}
-
     });
 
 
@@ -165,16 +126,8 @@ function setupggDOM (data) {
                     // getDataset();
                     var active_dataset = $(this).parents(".scope").find(".data").text(),
                         data_names = data.datasets[active_dataset];
-                        //current_name = $(this).text(),
-                        //name_pos = data_names.indexOf(current_name),
-                        //next_pos = name_pos + 1 > data_names.length - 1
-                        //    ? name_pos + 1 - data_names.length
-                        //    : name_pos + 1,
-                        //next_name = data_names[next_pos];
 
                     console.log("Clicked an aes argument that is a variable name.\n\tCurrent dataset: " + active_dataset + "\n\tAvailable names: " + data_names);
-                    //+ "\n\tCurrent name: " + current_name + "\n\tNext name: " + next_name);
-                    //$(this).text(next_name);
                     addDialog(event, data_names, this);
                 }
             }
