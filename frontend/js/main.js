7771333f983491e15ab128997d74fb8df288ff0e
LCHansson <love.hansson@gmail.com>
2015-03-30 23:45:32 +0200

    DOM changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -32,6 +32,10 @@ function addDialog(event, data, caller) {
     $dialog.css("left", x).css("top", y);
 }
 
+function addArgument(event, context, caller) {
+
+}
+
 $("#element-selector").click( function () {
     // Close the modal
     if($("#element-selector").is(':visible'))
@@ -76,87 +80,153 @@ function setupggDOM (data) {
 
 
     /* PARAMETERS */
-    $(".parameter").click(function(event) {
-        // Get scope
-        var parent = $(this).parents(".scope");
-        if (parent.attr('class') != "scope") {
-            console.log("Something appears to be wrong with the DOM; was really expecting the parent of this element to be a '<span class=\"scope\">'...");
-            return;
-        };
-        var scope = parent.attr('id');
+    function setParameterClicks() {
+        $(".parameter").unbind("click");
 
-        if (scope == "canvas") {
-            var active_geoms = $(".element .element-name.geom").text();
-            console.log("active geoms: " + active_geoms);
-            var allowed_aes = data.geom[active_geoms].aesthetics;
-            // TODO:
-            // - allowed_aes = active_geoms.forEach(function(x) { return data.geom[x].aesthetics; });
-        } else {
-            //    TODO:
-            //    - getLocallyScopedElement();
-            //    - getAllowedAes();
-        }
+        $(".parameter").click(function (event) {
+            // Get scope
+            var $scope = $(this).parents(".scope"),
+                envir = $(this).parent().attr("class"),
+                scope_id = $scope.attr('id');
 
-        console.log("Clicked an aes parameter.\n\tActive geoms: " + active_geoms + "\n\tAllowed aesthetics: " + allowed_aes);
 
-        //$(this).text(next_name);
-        addDialog(event, allowed_aes, this);
+            switch (envir) {
+                case "aes":
+                    if (scope_id == "canvas") {
+                        var geom_name = $(".geom").text();
+                        console.log("active geoms: " + geom_name);
 
-        // modifyAesArgument();
-        //var valid_arguments = getValidAesArguments(current_aesthetic);
-        //updateAesArgument(valid_arguments);
-    });
+                        var allowed_params = data.geom[geom_name].aesthetics;
+                    } else {
+                        //    TODO:
+                        //    - getLocallyScopedElement();
+                        //    - getAllowedAes();
+                        var geom_name = $(this).siblings(".geom").text(),
+                            allowed_params = data.geom[geom_name].args;
 
+                        console.log("parent geom: " + geom_name);
+                    }
+
+                    console.log("Clicked an aes parameter.\n\tGeom name(s): " + geom_name + "\n\tAllowed aesthetics: " + allowed_params);
+
+                    break;
+                case "element":
+                    var element_name = $(this).siblings(".element-name").text()
+                        allowed_params = data.geom[element_name].args;
+
+                    console.log(allowed_params);
+                    break;
+            }
+
+
+            //$(this).text(next_name);
+            addDialog(event, allowed_params, this);
+
+            // modifyAesArgument();
+            //var valid_arguments = getValidAesArguments(current_aesthetic);
+            //updateAesArgument(valid_arguments);
+        });
+    }
+
+    setParameterClicks();
 
     /* ARGUMENTS */
-    $(".argument").click(function(event) {
-        // Get scope
-        var parent = $(this).parents(".scope");
-        if (parent.attr('class') != "scope") { return; };
+    function setArgumentClicks() {
+        $(".argument").unbind("click");
 
-        var scope = parent.attr('id'),
-            parent_class = $(this).parent().attr('class');
+        $(".argument").click(function (event) {
+            // Get scope
+            var parent = $(this).parents(".scope");
+            if (parent.attr('class') != "scope") {
+                return;
+            }
 
-        //console.log("Parent class: " + parent_class)
+            var scope = parent.attr('id'),
+                parent_class = $(this).parent().attr('class');
 
-        // If parent is aes, make aes modifications.
-        if ( parent_class == 'aes-parameter') {
-            // Scope is global ('canvas')
-            if (scope == "canvas") {
-                if ($(this).attr('data-type') == "varname") {
-                    var active_dataset = $(this).parents(".scope").find(".data").text(),
-                        data_names = data.datasets[active_dataset];
+            console.log("Parent class: " + parent_class);
 
-                    console.log("Clicked an aes argument that is a variable name.\n\tCurrent dataset: " + active_dataset + "\n\tAvailable names: " + data_names);
-                    addDialog(event, data_names, this);
+            // If parent is aes, make aes modifications.
+            if (parent_class == 'aes') {
+                // Scope is global ('canvas')
+                if (scope == "canvas") {
+                    if ($(this).attr('data-type') == "varname") {
+                        var active_dataset = $(this).parents(".scope").find(".data").text(),
+                            data_names = data.datasets[active_dataset];
+
+                        console.log("Clicked an aes argument that is a variable name.\n\tCurrent dataset: " + active_dataset + "\n\tAvailable names: " + data_names);
+                        addDialog(event, data_names, this);
+                    }
+                }
+                // Scope is local (geom or stat)
+                else {
+                    console.log("Clicked an aes argument with local scope.");
                 }
             }
-            // Scope is local (geom or stat)
-            else {
-                console.log("Clicked an aes argument with local scope.");
-            }
-        }
 
-        // Parent is not aes, but element
-        else {
-            // Global scope
-            if (scope == "canvas") {
-                console.log("Clicked an element argument with global scope.");
-            }
-            // Local scope
+            // Parent is not aes, but element
             else {
-                console.log("Clicked an element argument with local scope.");
+                // Global scope
+                if (scope == "canvas") {
+                    console.log("Clicked an element argument with global scope.");
+                }
+                // Local scope
+                else {
+                    console.log("Clicked an element argument with local scope.");
+                }
             }
-        }
-    });
+        });
+    }
+
+    setArgumentClicks();
 
     /* ELEMENT */
-    $(".geom").click(function(event) {
-        var allowed_geoms = Object.getOwnPropertyNames(data.geom);
-        console.log(allowed_geoms);
+    function setElementClicks() {
+        $(".geom").unbind("click");
 
-        addDialog(event, allowed_geoms, this);
-    })
+        $(".geom").click(function (event) {
+            var allowed_geoms = Object.getOwnPropertyNames(data.geom);
+            console.log(allowed_geoms);
+
+            addDialog(event, allowed_geoms, this);
+        });
+    }
+
+    setElementClicks();
+
+
+    /* ADDERS */
+    $(".add").click(function(event) {
+        var scope = $(this).parents(".scope").attr("id"),
+            $context = $(this).parent(),
+            context_name = $context.attr("class");
+
+        switch (context_name) {
+            case "aes":
+                var number_of_siblings = $(this).siblings().length,
+                    before_insert = number_of_siblings > 1 ? ", " : "",
+                    new_par = "x",
+                    new_arg = "table";
+                break;
+            case "element":
+                var number_of_siblings = $(this).siblings().length,
+                    before_insert = number_of_siblings > 1 ? ", " : "",
+                    new_par = "test",
+                    new_arg = "test";
+                break;
+        }
+
+        $(this).before(before_insert +
+            '<span class="parameter modifiable interactive">' + new_par + '</span>\n' +
+            '<span class="equals">=</span>\n' +
+            '<span class="argument modifiable interactive" data-type="varname">' + new_arg + '</span>\n'
+        )
+
+        setParameterClicks();
+        setArgumentClicks();
+
+        console.log("Clicked add element.\n\tscope: " + scope + "\n\tcontext: " + context_name);
+    });
 }
 
 
@@ -178,7 +248,9 @@ $(function() {
 
     $("#plotbutton").on("click", function(e) {
         // Build ggplot command
-        var plotcode = $("#ggplotCode").text().replace(/[\n\t ]+/g, "");
+        var plotcode = $("#ggplotCode").text()
+            .replace(/[\n\t ]+/g, "")
+            .replace(/[\u2026]/g, "");
         console.log(plotcode);
 
         e.preventDefault();
